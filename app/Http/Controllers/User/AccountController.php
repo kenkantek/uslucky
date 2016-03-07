86494ad6692a514a1348010ff4e1d04dbae3a6ee
Phu Hoa Doan <hoadp.vn@gmail.com>
2016-03-07 13:58:45 +0700

    active_user

--- app/Http/Controllers/User/AccountController.php
+++ app/Http/Controllers/User/AccountController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\AccountRequest;
 use App\Http\Requests\AvatarRequest;
 use App\Http\Requests\PasswordRequest;
-
+use App\Models\User;
 
 class AccountController extends Controller
 {
@@ -14,6 +14,8 @@ class AccountController extends Controller
     {
         parent::__construct();
         $this->middleware('auth');
+        $this->middleware('active', ['except' => ['getThank', 'getVerify', 'getReSendEmail']]);
+        // $this->middleware('guest', ['except' => 'getVerify']);
     }
 
     public function putEditAccount(AccountRequest $request)
@@ -21,14 +23,14 @@ class AccountController extends Controller
         $user = $this->user;
 
         $user->first_name = $request->first_name;
-        $user->last_name = $request->last_name;
-        $user->phone = $request->phone;
-        $user->birthday = $request->birthday . " 00:00:00";
-        $user->address = $request->address;
-        $user->country = $request->country;
-        $user->state = $request->state;
-        $user->city = $request->city;
-        $user->zipcode = $request->zipcode;
+        $user->last_name  = $request->last_name;
+        $user->phone      = $request->phone;
+        $user->birthday   = $request->birthday . " 00:00:00";
+        $user->address    = $request->address;
+        $user->country    = $request->country;
+        $user->state      = $request->state;
+        $user->city       = $request->city;
+        $user->zipcode    = $request->zipcode;
 
         $user->save();
 
@@ -37,14 +39,14 @@ class AccountController extends Controller
 
     public function postChangeAvatar(AvatarRequest $request)
     {
-        $avatar = $this->user;
+        $avatar     = $this->user;
         $path_image = 'uploads/avatar/';
 
         if (\File::exists($avatar->avatar)) {
             \File::delete($avatar->avatar);
         }
 
-        $file = $request->file('avatar');
+        $file  = $request->file('avatar');
         $image = time() . '-' . $file->getClientOriginalName();
         $file->move($path_image, $image);
         $avatar->avatar = $path_image . $image;
@@ -62,9 +64,55 @@ class AccountController extends Controller
 
             return $password;
 
-        }
-        else {
+        } else {
             return response(['old_password' => 'Your old password incorrect!'], 401);
+        }
+    }
+
+    public function getVerify($active_code)
+    {
+        $user = User::whereActiveCode($active_code)->first();
+        if (!$user) {
+            return redirect()->route('front::settings.account');
+        } elseif ($this->user->active == 1) {
+            return redirect()->route('front::settings.account');
+        } else {
+            $user->active_code = null;
+            $user->active      = 1;
+            $user->save();
+            $flash_mes = 'verify';
+            return view('user.verify', compact('flash_mes'));
+        }
+
+    }
+
+    public function sendFailedLoginResponse()
+    {
+        return response(['message' => 'Your email or password does not match!'], 401);
+    }
+
+    public function getThank()
+    {
+        if ($this->user->active == 1) {
+            return redirect()->route('front::settings.account');
+        } else {
+            $flash_mes = 'success';
+            return view('user.verify', compact('flash_mes'));
+        }
+    }
+
+    public function getReSendEmail()
+    {
+        if ($this->user->active == 1) {
+            return redirect()->route('front::settings.account');
+        } else {
+            $user = $this->user;
+            \Mail::send('verify', ['event' => $user], function ($message) use ($user) {
+                $message->from(env('MAIL_FROM'), env('MAIL_FROM_NAME'));
+                $message->to($user->email)->subject('Verify your email address');
+            });
+            $flash_mes = 'resent'
+            return view('user.verify', compact('flash_mes'));
         }
     }
 }
---
+++ app/Listeners/UserListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserCreated;
+use Mail;
+
+class UserListener
+{
+
+    /**
+     * @param $event
+     */
+    public function onUserCreated($event)
+    {
+        Mail::send('verify', ['event' => $event->user], function ($m) use ($event) {
+            $m->from('hello@app.com', 'Verify Your Email Address From US Lucky');
+
+            $m->to($event->user->email, $event->user->first_name)->subject('Verify Your Email Address');
+        });
+    }
+
+    public function subscribe($events)
+    {
+        $events->listen(
+            \App\Events\UserCreated::class,
+            static::class . '@onUserCreated'
+        );
+    }
+}
