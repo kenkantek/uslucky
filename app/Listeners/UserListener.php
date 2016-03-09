6dc69264b93cba0e8f2fe5654551c5f719cbe02c
Phu Hoa Doan <hoadp.vn@gmail.com>
2016-03-09 11:08:57 +0700

    fix MAIL_FROM

--- app/Listeners/UserListener.php
+++ app/Listeners/UserListener.php
@@ -14,8 +14,7 @@ class UserListener
     public function onUserCreated($event)
     {
         Mail::send('mail.verify', ['event' => $event->user], function ($m) use ($event) {
-            $m->from('hello@app.com', 'Verify Your Email Address From US Lucky');
-
+            $m->from(env('MAIL_FROM'), 'Verify Your Email Address From US Lucky');
             $m->to($event->user->email, $event->user->first_name)->subject('Verify Your Email Address');
         });
     }
