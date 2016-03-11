7fbede997cf8357a035a342a1afedf358210262c
Phu Hoa Doan <hoadp.vn@gmail.com>
2016-03-11 18:29:47 +0700

    fix css, email

--- resources/views/mail/payments/claim.blade.php
+++ resources/views/mail/payments/claim.blade.php
@@ -10,7 +10,24 @@
     @include('beautymail::templates.sunny.contentStart')
 
         <p>This is transaction history #{{ $transaction->id }}</p>
-        Table detail here.
+        <table class="table table-bordered table-hover" style="border:1px solid #aaa; width: 100%">
+            <thead style="border:1px solid #aaa;">
+                <tr>
+                    <th style="border:1px solid #aaa">Date claim</th>
+                    <th style="border:1px solid #aaa">Money request</th>
+                    <th style="border:1px solid #aaa">Your blance</th>
+                    <th style="border:1px solid #aaa">Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="border:1px solid #aaa">{{$transaction->created_at}}</td>
+                    <td style="border:1px solid #aaa">{{$transaction->amount}}</td>
+                    <td style="border:1px solid #aaa">{{$transaction->amount_total}}</td>
+                    <td style="border:1px solid #aaa">Pending</td>
+                </tr>
+            </tbody>
+        </table>
     @include('beautymail::templates.sunny.contentEnd')
 
     @include('beautymail::templates.sunny.button', [
