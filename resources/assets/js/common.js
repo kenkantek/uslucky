8bc2559c1ab697a05a0ef0589b5f3029261b8f05
tronghiep92 <tronghiep92@gmail.com>
2016-03-18 13:55:15 +0700

    count time down

--- resources/assets/js/common.js
+++ resources/assets/js/common.js
@@ -1,3 +1,7 @@
+import moment from 'moment';
+import countdown from 'moment-countdown';
+import tz from 'moment-timezone';
+
 module.exports = {
 	alertSuccess(message = 'Ya!', caption = "Successful.") {
 		swal(message, caption, 'success');
@@ -5,5 +9,14 @@ module.exports = {
 
 	alertError(message = 'Ooop!', caption = "Something wrong!") {
 		swal(message, caption, 'error');
-	}
+	},
+
+    countDown(date_draw = new Date(), addHour = hours_before_close, tz = _timzone) {
+        let end_month = date_draw.getMonth() + 1,
+            now = moment().tz(tz).format(),
+            start = moment(String(now).replace('T', ' ').substr(0, 19)),
+            end = `${date_draw.getFullYear()}-${end_month >= 10 ? end_month : '0' + end_month}-${date_draw.getDate()} ${addHour}:00:00`;
+            // console.log(String(now).replace('T', ' ').substr(0, 19), end);
+        return start.countdown(end).toString();
+    }
 }
