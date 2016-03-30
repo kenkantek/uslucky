eb39c40e486371da413abfeffad6818dfee5369f
Phu Hoa Doan <hoadp.vn@gmail.com>
2016-03-30 21:25:01 +0700

    email to admin

--- app/Listeners/OrderListener.php
+++ app/Listeners/OrderListener.php
@@ -19,6 +19,7 @@ class OrderListener
             $m->from(env('MAIL_FROM'), env('MAIL_FROM_NAME'));
             $m->to($user->email, $user->fullname)->subject(env('TITLE') . " - Your order was purchased");
         });
+
     }
 
     public function onOrderDeleted($event)
@@ -38,6 +39,11 @@ class OrderListener
         Mail::send('mail.order.neworder', ['senderName' => $user->fullname, 'order' => $order, 'logo' => ['path' => 'http://www.dadafest.co.uk/wp-content/uploads/2011/12/big-lottery-fund-logo.gif', 'width' => 150, 'height' => 150]], function ($m) use ($user) {
             $m->from(env('MAIL_FROM'), env('MAIL_FROM_NAME'));
             $m->to($user->email, $user->fullname)->subject("Your new order from USLUCKY");
+        });
+
+        Mail::send('mail.order.toadmin', ['senderName' => 'Ken,', 'order' => $order], function ($m) use ($user) {
+            $m->from(env('MAIL_FROM'), env('MAIL_FROM_NAME'));
+            $m->to('hoadp.vn@gmail.com', $user->fullname)->subject($user->fullname . " just bought a ticket");
         });
 
     }
