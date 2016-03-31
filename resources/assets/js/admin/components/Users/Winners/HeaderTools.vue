fe03a0cc63f53c26542cc8d42dbedc08f55d35e5
Phu Hoa Doan <hoadp.vn@gmail.com>
2016-03-31 18:44:57 +0700

    winners

--- resources/assets/js/admin/components/Users/Winners/HeaderTools.vue
+++ resources/assets/js/admin/components/Users/Winners/HeaderTools.vue
@@ -3,20 +3,23 @@
         <slot name="header"></slot>
         <div class="table-group-actions" style="clear:both; margin-bottom:25px">
             <div class="row">
-                <div class="col-xs-12 col-md-3">
-                    <label>Draw Date:</label>
-                    <div class="datetime-picker">
-                        <select class="form-control" v-model="game">
-                            <option v-for="date in dates" v-text="date.drawTime"></option>
-                        </select>
-                    </div>
-                </div>
                 <div class="col-xs-12 col-md-2">
                     <label>Game Type:</label>
                     <select class="form-control" v-model="game">
                         <option v-for="game in games" :value="game.id" v-text="game.name"></option>
                     </select>
                 </div>
+                
+                <div class="col-xs-12 col-md-3">
+                    <label>Draw Date:</label>
+                    <select class="form-control" v-model="drawAt">
+                        <option 
+                        v-for="result in results |  filterBy game in 'game_id'" 
+                        v-text="result.draw_at" :value="result.id">
+                        </option>
+                    </select>
+                </div>
+                
                 <div class="col-xs-12 col-md-3">
                     <button class="btn btn-sm green table-group-action-submit move-top--25" @click="onSearch">
                         <i class="fa fa-check"></i> Search
@@ -28,11 +31,20 @@
 </template>
 
 <script>
-import moment from 'moment';
+    import _ from 'lodash';
 
     export default {
-        
-        props: ['game', 'games', 'dates'],
+        props: ['game', 'games', 'drawAt'],
+
+        data() {
+            return {
+                results: [],
+            }
+        },
+
+        ready() {
+            this.results = _results;
+        },
 
         methods: {
             onSearch() {
