e91712927f45e961deba8796ebed9f70bd63d1af
tronghiep92 <tronghiep92@gmail.com>
2016-05-03 06:28:09 +0000

    upate lang 03.05.2016

--- resources/lang/cn/megamillions.php
+++ resources/lang/cn/megamillions.php
@@ -3,4 +3,5 @@
 return [
     'title' => 'Buy Official Tickets for U.S. Mega Millions Lottery Online',
     'des'   => 'Select 1-75 main numbers and 1-15 for the Mega Ball to <strong>buy official Mega Millions tickets</strong> every Tuesday and Friday. Mega Millions has a no-limit jackpot, rising as much as $656 million in March 2012. Play the Mega Millions lottery online and stand a chance to <strong>become a mega-millionaire!</strong>',
+
 ];
---
+++ resources/lang/cn/play.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'luckynumber'         => 'MY LUCKY NUMBERS',
+    'quick_pick'          => 'Quick Pick',
+    'per_line_extra'      => 'For An Extra $:extra Per Ticket, Make It A Power Play',
+    'per_line'            => 'Ticket Price',
+    'extra'               => 'Extra',
+    'play_now'            => 'Play now',
+    'total'               => 'Total',
+    'choose'              => 'Choose',
+
+    'credit'              => 'CARD NUMBER',
+    'credit_expiration'   => 'EXPIRATION',
+    'credit_exp'          => 'EXP',
+    'credit_date'         => 'DATE',
+    'credit_cvc'          => 'CVC',
+    'valid_card'          => 'Valid Card Number',
+
+    'modal_title'         => 'Purchase',
+    'message'             => 'You need login to Purchase ticket.',
+    'message_err'         => 'Something wrong, please try again!',
+
+    'modal_choose'        => 'Choose method payment',
+    'modal_balance'       => 'Account balance',
+    'modal_afford'        => 'You can not afford to buy it.',
+    'modal_button_add'    => 'Add now',
+    'modal_credit'        => 'By credit card',
+    'modal_des_credit'    => 'Description',
+    'modal_button_cancel' => 'Cancel',
+    'modal_button_submit' => 'Submit',
+    'modal_swal'          => 'Are you sure purchase it?',
+    'click_here_login'    => "Click <a @click.stop='closeModal' href=':redirect'>{here</a> and try again.",
+    'estimated_jackpot'   => 'ESTIMATED JACKPOT',
+];
