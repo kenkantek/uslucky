0ab5c2d3266a68a4381585ed240713213a8b629e
KanTek <kantekcom@gmail.com>
2016-08-05 11:03:33 +0800

    bk web live

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -10,25 +10,34 @@
         <a href="#" class="link">
             <header>{{ trans('footer.box_1') }}</header>
             <p>
-                <img src="{{asset('images/security.jpg')}}" style="max-width: 100%" alt="">
+                <img src="{{asset('images/security.png')}}" style="max-width: 100%" alt="">
             </p>
         </a>
         <a href="#" class="link">
             <header>{{ trans('footer.box_2') }}</header>
             <p>
-                <img src="{{asset('images/payment.jpg')}}" style="max-width: 100%" alt="">
+                <img src="{{asset('images/payment.png')}}" style="max-width: 100%" alt="">
             </p>
         </a>
         <a href="#" class="link">
             <header>{{ trans('footer.box_3') }}</header>
             <p>
-                <img src="{{asset('images/contact us.jpg')}}" style="max-width: 100%" alt="">
+                <img src="{{asset('images/contact_us.png')}}" style="max-width: 100%" alt="">
             </p>
         </a>
     </div>
     <div class="footer">
+	<a href="#" target="_blank">
+            {{ trans('footer.copyright_1') }}
+        </a>|
+	<a href="#" target="_blank">
+            {{ trans('footer.copyright_2') }}
+        </a>|
+	<a href="#" target="_blank">
+            {{ trans('footer.copyright_3') }}
+        </a>|
         <a href="{{ trans('footer.powered_by_link') }}" target="_blank">
             {{ trans('footer.powered_by') }}
-        </a>
+        </a>    
     </div>
 </footer>
